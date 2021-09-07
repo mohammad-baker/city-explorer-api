6a75e7312d4f0d3227010b6d0f1988af3649ec28
mohammad <altaeabbaker@gmail.com>
2021-09-08 01:01:24 +0300

    add lab 07

--- server.js
+++ server.js
@@ -1,2 +1,58 @@
-"use strict";
-console.log("hu");
+ "use strict";
+
+
+const express = require('express') ;
+const app = express();
+const cors = require('cors');
+app.use(cors());
+ require('dotenv').config();
+const PORT = process.env.PORT;
+const dataWeather = require('./data/weather.json')
+app.get('/weatherdata', (request, response) => {
+    const cityName = request.query.city_name;
+    
+    const cityLat = request.query.lat;
+    const cityLon = request.query.lon;
+    if (cityName) {
+      const selecteData = dataWeather.find((element) => {
+        return element.city_name === cityName;
+      });
+      if (selecteData) {
+
+
+        class Forcast {
+
+            constructor(data, description) {
+              this.data = data;
+              this.description = description;
+            }
+          }
+          selectedCityData.map((element) => {
+            let object = new Forcast(
+  
+              element.datetime,
+              element.weather.description
+            );
+        let selectedCityData = selecteData.data;
+              
+        let array = [];
+       
+
+
+        
+
+
+
+          array.push(object);
+        });
+        response.json(array);
+      } else {
+        response.send('The City is not defiend');
+      }
+    } else {
+      response.send('Error : You Have Not Entered Any Data!');
+    }
+  });
+  app.listen(PORT, () => {
+    console.log(`Server started on port`,PORT);
+  });
