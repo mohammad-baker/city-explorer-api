df2ef6d84be0ef2ecf6efad176b3a2242fabc24e
mohammad <altaeabbaker@gmail.com>
2021-09-08 03:16:30 +0300

    As

--- server.js
+++ server.js
@@ -4,22 +4,37 @@
 const express = require('express') ;
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
 app.use(cors());
  require('dotenv').config();
 const PORT = process.env.PORT;
+
+require('dotenv').config();
+const WEATHER_API_KEY =process.env.WEATHER_API_KEY
 const dataWeather = require('./data/weather.json')
-app.get('/weatherdata', (request, response) => {
-    const cityName = request.query.city_name;
+app.get('/weather', (request, response) => {
+ const lat= request.query.lat;
+ const lon= request.query.lon;
+const urlWeather = 'https://api.weatherbit.io/v2.0/forecast/daily'
+try{
+const weatherReson = await axios.get(`${urlWeather}?lat=${lat}&lon=${lon}&key={WEATHER_API_KEY}`)
     
-    const cityLat = request.query.lat;
+response.json(weatherReson.data);
+}catch(error){
+  response.json(error.data);
+}
+const cityName12 = weatherReson.data;
+    
+    // const cityLat = request.query.lat;
     const cityLon = request.query.lon;
     if (cityName) {
-      const selecteData = dataWeather.find((element) => {
-        return element.city_name === cityName;
-      });
-      if (selecteData) {
+      // const selecteData = dataWeather.find((element) => {
+      //   return element.city_name === cityName;
+      // });
+      if (cityName12) {
 
-
+        const selecteData =cityName12.data;
+        let array = [];
         class Forcast {
 
             constructor(data, description) {
@@ -27,15 +42,15 @@ app.get('/weatherdata', (request, response) => {
               this.description = description;
             }
           }
-          selectedCityData.map((element) => {
+          selecteData.map((element) => {
             let object = new Forcast(
   
               element.datetime,
               element.weather.description
             );
-        let selectedCityData = selecteData.data;
+        // let selectedCityData = selecteData.data;
               
-        let array = [];
+        // let array = [];
        
 
 
