9d1803027b535bdcbe0e2390c473455f88fd5865
mohammad <altaeabbaker@gmail.com>
2021-09-09 02:40:51 +0300

    lab07

--- server.js
+++ server.js
@@ -1,75 +1,61 @@
- "use strict";
+"use strict";
 
 
 const express = require('express') ;
 const app = express();
 const cors = require('cors');
 const axios = require('axios');
+require('dotenv').config();
 app.use(cors());
- require('dotenv').config();
+ 
 const PORT = process.env.PORT;
 
-require('dotenv').config();
-const WEATHER_API_KEY =process.env.WEATHER_API_KEY
-const dataWeather = require('./data/weather.json')
+
+
+const weather = require('./data/weather.json')
 app.get('/weather', (request, response) => {
- const lat= request.query.lat;
- const lon= request.query.lon;
-const urlWeather = 'https://api.weatherbit.io/v2.0/forecast/daily'
-try{
-const weatherReson = await axios.get(`${urlWeather}?lat=${lat}&lon=${lon}&key={WEATHER_API_KEY}`)
-    
-response.json(weatherReson.data);
-}catch(error){
-  response.json(error.data);
-}
-const cityName12 = weatherReson.data;
-    
-    // const cityLat = request.query.lat;
-    const cityLon = request.query.lon;
-    if (cityName) {
-      // const selecteData = dataWeather.find((element) => {
-      //   return element.city_name === cityName;
-      // });
-      if (cityName12) {
+  const city = request.query.city_name;
+  const lat = request.query.lat;
+  const lon = request.query.lon;
 
-        const selecteData =cityName12.data;
-        let array = [];
-        class Forcast {
-
-            constructor(data, description) {
-              this.data = data;
-              this.description = description;
-            }
-          }
-          selecteData.map((element) => {
-            let object = new Forcast(
-  
-              element.datetime,
-              element.weather.description
-            );
-        // let selectedCityData = selecteData.data;
-              
-        // let array = [];
-        // let array = [];
-        // let array = [];
-       
-
-
-        
-
-
-
-          array.push(object);
-        });
-        response.json(array);
-      } else {
-        response.send('there is no city');
+  if (city) {
+    const select = weather.find((element) => {
+      return element.city_name.toLowerCase() === city;
+    });
+    if (select) {
+      let selectedData = select.data;
+      let array = [];
+      class Forcast {
+        constructor(data, description) {
+          this.data = data;
+          this.description = description;
+        }
       }
-    } else {
-      response.send('there is no data');
+      selectedData.map((element) => {
+        let object = new Forcast(
+          element.datetime,
+          element.weather.description
+        );
+
+        array.push(object);
+
+
+
+      });
+      response.json(array);
+
+
+    } 
+    else {
+      response.send('that city is not there');
     }
-  });
-  app.listen(PORT, () => {
-    console.log(`Server started on port`,PORT);
-  });
+  } 
+  
+  else {
+    response.send('there is no city');
+  }
+
+
+});
+app.listen(PORT, () => {
+});
