9d715173ae1a17517c8a60a9a0415be5f0bafbd1
mohammad <altaeabbaker@gmail.com>
2021-09-12 00:47:35 +0300

    add 10

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -3,20 +3,42 @@ const axios = require('axios');
 require('dotenv').config();
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const WeatherOFcity = require('../models/weather.model');
-
+const Cache = require('../helper/cache.helper')
+let cacheObject = new Cache();
 const citweather = async (req, res) => {
 
   const city = req.query.city;
+if (cacheObject.forCast.length){
+  const findData = cacheObject.forCast.find(location => location.city === city)
 
+  const dayinmilSec =50000;
+  const oneDayPassed =(Date.now()-cacheObject.time)> dayinmilSec;
+  if(oneDayPassed){
+    cacheObject = new Cache;
+  }
+  if(findData){
+    res.json(findData.array)
+  }
+
+}else{
   const weatherBitResponse = await axios.get(
     `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${WEATHER_API_KEY}`
   );
 
   if (city) {
     let array = weatherBitResponse.data.data.map((value) => {
-      return new WeatherOFcity((value.datetime,value.weather.description))})
+      return new WeatherOFcity((`Low of ${data1.low_temp}, high of ${data1.high_temp} with ${data1.weather.description} `,
+      ` ${data1.datetime}`))})
 
     if (array) {
+
+
+      cacheObject.forCast.push({
+        "city":city,
+        "data":array
+      })
+      return array
+
       res.json(array);
      
       
@@ -26,6 +48,8 @@ const citweather = async (req, res) => {
   } else {
     res.json('no city');
   }
+}
+  
 };
 
 module.exports = citweather;
